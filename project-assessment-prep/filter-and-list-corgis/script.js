b20b71277069caff71e881f967aee92508e1e197
rwparrish <60946750+rwparrish@users.noreply.github.com>
2023-09-26 16:15:09 -0600

    add filter-corgis and filter books to assessment prep

---
+++ project-assessment-prep/filter-and-list-corgis/script.js
@@ -0,0 +1,11 @@
+// Challenge:
+// Using the given dogs array, create a variable named "corgis" whose value is an array containing only the dogs of breed Corgi
+// When a user clicks the button "Corgis!!",
+// Use the corgis variable to add only the dogs with the breed "Corgi" to the Corgi Container
+
+// Use these dogs:
+const dogs = [
+    { name: "Cheddar", breed: "Corgi" },
+    { name: "Toto", breed: "Terrier" },
+    { name: "Susan", breed: "Corgi" },
+];
---
+++ project-assessment-prep/filter-books/script.js
@@ -0,0 +1,43 @@
+/* When the button is clicked, use the filter method to filter the books array and create a new array named filteredBooks 
+that contains only the books in the genre that the user entered - this shuld be case insensitive.
+Then, call the renderBookList function with your new array.
+*/
+
+const books = [
+    { title: 'To Kill a Mockingbird', author: 'Harper Lee', genre: 'Fiction' },
+    { title: '1984', author: 'George Orwell', genre: 'Dystopian Fiction' },
+    { title: 'Pride and Prejudice', author: 'Jane Austen', genre: 'Classic Fiction' },
+    { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', genre: 'Classic Fiction' },
+    { title: 'The Catcher in the Rye', author: 'J.D. Salinger', genre: 'Fiction' },
+    { title: 'Harry Potter and the Sorcerer\'s Stone', author: 'J.K. Rowling', genre: 'Fantasy' },
+    { title: 'The Lord of the Rings', author: 'J.R.R. Tolkien', genre: 'Fantasy' },
+    { title: 'To the Lighthouse', author: 'Virginia Woolf', genre: 'Modernist Fiction' },
+    { title: 'The Hobbit', author: 'J.R.R. Tolkien', genre: 'Fantasy' },
+    { title: 'Moby-Dick', author: 'Herman Melville', genre: 'Classic Fiction' },
+    { title: 'The Alchemist', author: 'Paulo Coelho', genre: 'Fiction' },
+    { title: 'The Odyssey', author: 'Homer', genre: 'Epic Poetry' },
+  ];
+  
+document.addEventListener("DOMContentLoaded", () => {
+    renderBookList(books)
+})
+
+function filterBooks(genre) {
+// Implement your logic here
+// Use the filter method to filter the books array
+// Return the filtered array
+}
+
+function renderBookList(books) {
+    const bookList = document.getElementById('book-list')
+    bookList.innerHTML = ''
+
+    books.forEach(book => {
+        const li = document.createElement('li')
+        li.innerText = `${book.title} by ${book.author} (${book.genre})`
+        bookList.appendChild(li);
+    });
+}
+
+  
+  
