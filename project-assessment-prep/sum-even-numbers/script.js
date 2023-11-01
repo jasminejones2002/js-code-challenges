2b563578db4684dd69d55bfa0d212b222cd6209d
rwparrish <60946750+rwparrish@users.noreply.github.com>
2023-11-01 14:40:20 -0600

    retool directions in sum-even-numbers

--- project-assessment-prep/sum-even-numbers/script.js
+++ project-assessment-prep/sum-even-numbers/script.js
@@ -5,15 +5,16 @@ Your function should:
 
 1. Iterate through the given numbers array using the filter method to check if each number is even
 2. Then sum the even numbers from the filtered array and return their sum
+3. Test your function with an example array.
 
-3. BONUS: Use the reduce method to sum the even numbers from the array
-
-*/
-console.log("hi")
-// Test your function with an example array.
-const numbers = [2, 5, 8, 10, 3, 6];
 const result = sumEvenNumbers(numbers);
-console.log('Sum of even numbers:', result); // The result should be 26.
+console.log('Sum of even numbers:', result); --> The result should be 26.
+
+BONUS: Use the reduce method to sum the even numbers from the array - try this after you have completed this exercise
+*/
+
+const numbers = [2, 5, 8, 10, 3, 6];
+
   
 
 
