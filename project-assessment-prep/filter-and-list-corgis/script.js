2b36c1383cde5fb08d4732e0a4e7f74671f52eef
rwparrish <60946750+rwparrish@users.noreply.github.com>
2023-11-02 16:54:42 -0600

    format directions

--- project-assessment-prep/filter-and-list-corgis/script.js
+++ project-assessment-prep/filter-and-list-corgis/script.js
@@ -1,6 +1,8 @@
-// Using the given dogs array, create a variable named "corgis" whose value is an array containing only the dogs of breed Corgi.
-// When a user clicks the button "Corgis!!",
-// Use the corgis variable to add only the dogs with the breed "Corgi" to the Corgi Container - List the dogs' names in the Corgi Container
+/* 
+Using the given dogs array, create a variable named "corgis" whose value is an array containing only the dogs of breed Corgi.
+When a user clicks the button "Corgis!!",
+Use the corgis variable to add only the dogs with the breed "Corgi" to the Corgi Container - List the dogs' names in the Corgi Container
+*/
 
 // Use these dogs:
 const dogs = [
--- project-assessment-prep/product-search/script.js
+++ project-assessment-prep/product-search/script.js
@@ -1,11 +1,11 @@
 /* 
-  Create a search feature. When the user makes a change to the input field,
-  capture the user input and use it to filter through the product names - this should happen in real time.
-  Then list the products on the page that pass the filter - this should be case insensitive. 
-  You should allow products that contain all of the letters in order in the input to pass the filter. 
-  Use a h3 tag for the product name and a p tag for the category. 
-  Take a look at the HTML file to see where you should be appending your elements.
-  */
+Create a search feature. When the user makes a change to the input field,
+capture the user input and use it to filter through the product names - this should happen in real time.
+Then list the products on the page that pass the filter - this should be case insensitive. 
+You should allow products that contain all of the letters in order in the input to pass the filter. 
+Use a h3 tag for the product name and a p tag for the category. 
+Take a look at the HTML file to see where you should be appending your elements.
+*/
 
 const products = [
     { id: 1, name: "Apple", category: "Fruits" },
