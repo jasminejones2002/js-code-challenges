9795b931069c2dfb44c6d2c874ae803ac5546006
jasminejones2002 <jazziejones02@yahoo.com>
2024-02-04 05:15:34 -0600

    random words count complete

--- extra-challenges/random-words-count/script.js
+++ extra-challenges/random-words-count/script.js
@@ -9,3 +9,20 @@ const str = "Gabba boo dah ugh fpp BOO gabba doo Boo"
 console.log(convertToObjectWithCounts(str)) => { gabba: 2, boo: 3, dah: 1, ugh: 1, fpp: 1, doo: 1 }
 
 */
+
+function randomWords(string) {
+    const words = string.split(' ')
+    const obj = {}
+
+    for (let i = 0; i < words.length; i++) {
+        const word = words[i]
+        if (obj.hasOwnProperty(word)) {
+            obj[word] += 1;
+        } else {
+            obj[word] = 1
+        }
+    }
+    return obj
+}
+
+console.log(randomWords('Gabba boo dah ugh fpp BOO gabba doo Boo'))
